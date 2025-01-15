c4e2fb27ec861d3161b10f0eb9f0f714da289f24
Aj matasa <101048833+Sulemanringem@users.noreply.github.com>
2025-01-15 13:14:15 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -24,9 +24,16 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request).catch(() => {
-        console.error("Failed to fetch:", event.request.url);
-      });
+      return (
+        response ||
+        fetch(event.request).catch((error) => {
+          console.error("Failed to fetch:", event.request.url, error);
+          return new Response("Network error occurred", {
+            status: 408,
+            statusText: "Network Error",
+          });
+        })
+      );
     })
   );
 });
